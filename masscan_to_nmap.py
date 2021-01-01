0bd8c6024733b669c95bcdd0885fb5c5cf695a21
ValluvarSploit <73417621+ValluvarSploit@users.noreply.github.com>
2021-01-01 23:01:18 +0530

    Update masscan_to_nmap.py

--- masscan_to_nmap.py
+++ masscan_to_nmap.py
@@ -1,51 +1,70 @@
-#!/usr/bin/python
-#
-# import masscan output and run an nmap scan on the results
-#
+#!/usr/bin/env python3
 
-import sys
-import argparse
-
-from libnmap.parser import NmapParser, NmapParserException
+#import argparse
 from libnmap.process import NmapProcess
+from libnmap.parser import NmapParser, NmapParserException
+from time import sleep, strftime, localtime
 from collections import defaultdict
-from time import sleep
 
-parser = argparse.ArgumentParser(description='Import masscan results and run an nmap scan against the target.')
-parser.add_argument('-f', '--scan-file', dest='scan_file', help='Masscan XML file')
-#parser.add_argument('-f', '--scan_file', nargs='?', type=argparse.FileType('r'), help='XML file containing masscan results.')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(description='Import masscan results and run an nmap scan against the target.')
+#parser.add_argument('-f', '--scan_file', nargs='?', type=argparse.FileType('r'))
+#args = parser.parse_args()
 
 def do_scan(target, options):
-    print(target, options)
+    parsed = None
     nmproc = NmapProcess(target, options)
-    rc = nmproc.sudo_run_background()
-
+    rc = nmproc.run()
+    
     while nmproc.is_running():
-#        print("Nmap scan running: ETC: {0} DONE: {1}%".format(nmproc.etc,nmproc.progress))
-        print(nmproc.stdout)
-        sleep(2)
+        print('Nmap scan running: ETC: {0} DONE: {1}%'.format(nmproc.etc, nmproc.progress))
+        sleep(10)
 
-    if (rc !=0):
-        print("Nmap scan failed: {0}".format(nmproc.stderr))
+    if nmproc.rc != 0:
+        print('nmap scan failed: {0}'.format(nmproc.stderr))
     
     try:
         parsed = NmapParser.parse(nmproc.stdout)
     except NmapParserException as e:
-        print("Scan parsing failed: {0}".format(e.msg))
+        print('Scan parsing failed: {0}'.format(e.msg))
 
-    print("rc: {0} output: {1}".format(nmproc.rc, nmproc.summary))
     return parsed
 
-def main():
+def print_scan(nmap_report):
+    print('Starting Nmap {0} ( http:nmap.org ) at {1}'.format(
+        nmap_report.version, 
+        strftime('%d-%m-%Y %H:%M', localtime(int(nmap_report.started)))))
     
-    # space on the end is on purpose
-    options = "-sV -pT:"
+    for host in nmap_report.hosts:
+        if len(host.hostnames):
+            tmp_host = host.hostnames.pop()
+        else:
+            tmp_host = host.address
+
+        print('Nmap scan report for {0} ({1})'.format(
+            tmp_host, 
+            host.address))
+        print('Host is {0}.'.format(host.status))
+        print('  PORT     STATE         SERVICE')
+
+        for serv in host.services:
+            pserv = '{0:>5s}/{1:3s}  {2:12s}  {3}'.format(
+                str(serv.port),
+                serv.protocol,
+                serv.state,
+                serv.service)
+            if len(serv.banner):
+                pserv += ' ({0})'.format(serv.banner)
+            print(pserv)
+    print(nmap_report.summary)
+
+
+def main():
+    options = '-sV -p '
 
     try:
-        report = NmapParser.parse_fromfile(args.scan_file)
+        report = NmapParser.parse_fromfile('masscan.xml')
     except NmapParserException as e:
-        print("Scan parsing failed: {0}".format(nmproc.stderr))
+        print("Scan parsing failed: {0}".format(e.msg))
 
     report_dict = defaultdict(list)
     for host in report.hosts:
@@ -53,9 +72,14 @@ def main():
             report_dict[host.ipv4].append(service.port)
 
     for target, ports in report_dict.items():
-        all_port = ','.join(map(str, ports))
-        print("Getting ready to scan {0} on ports {1}".format(target, all_port))
-        do_scan(target, options + str(all_port))
+        ports_formatted = ','.join(map(str, ports))
+        print("Getting ready to scan {0} on ports {1}".format(target, ports_formatted))
+        report = do_scan(target, options + ports_formatted)
 
-if __name__ == '__main__':
+        if report:
+            print_scan(report)
+        else:
+            print('No results returned')
+
+if __name__ == "__main__":
     main()
