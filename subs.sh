dc59064f051a2b0b93032225f3a583afd16c8bcc
ValluvarSploit <73417621+ValluvarSploit@users.noreply.github.com>
2021-05-05 16:02:33 +0530

    Update subs.sh

--- subs.sh
+++ subs.sh
@@ -6,7 +6,8 @@ TARGET=$1
 #python3 subscraper/subscraper.py "$TARGET" -o subscraper.txt;
 
 echo 'Sublist3r Enumeration'
-python3 Sublist3r/sublist3r.py -d "$TARGET" -t 10 -v -o sublist3r.txt > /dev/null          
+#python3 Sublist3r/sublist3r.py -d "$TARGET" -t 10 -v -o sublist3r.txt > /dev/null   
+python3 Sublist3r/sublist3r.py -d "$TARGET" -t 10 -v
 
 echo 'Amass Enumeration'
 ./amass_linux_amd64/amass enum -passive -d "$TARGET" -rf resolvers.txt -o amass-passive.txt ;
@@ -18,7 +19,8 @@ echo 'Subfinder Enumeration'
 ./subfinder -d "$TARGET" -all -o subfinder-key.txt -rL resolvers.txt ; 
 
 echo 'Findomain Enumeration'
+chmod +x findomain
 ./findomain -t "$TARGET" -q | tee findomain.txt; 
 
-cat subscraper.txt sublist3r.txt amass-passive.txt assetfinder.txt subfinder-key.txt findomain.txt >> recursive-subdomains.txt
+cat sublist3r.txt amass-passive.txt assetfinder.txt subfinder-key.txt findomain.txt >> recursive-subdomains.txt
 sort -u recursive-subdomains.txt -o recursive-subdomains.txt
