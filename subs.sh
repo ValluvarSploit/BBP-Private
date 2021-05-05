bd3e95710cb4105a9994d0f7ec2262e596cf86de
ValluvarSploit <73417621+ValluvarSploit@users.noreply.github.com>
2021-05-05 15:34:57 +0530

    Update subs.sh

--- subs.sh
+++ subs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 TARGET=$1
-sudo apt-get install python3-setuptools;
-wget -q -i requirements/subdomain-enum.txt; mv findomain-linux findomain; unzip amass_linux_amd64.zip; tar -xzvf subfinder_2.4.7_linux_amd64.tar.gz;
-go env -w GOPATH=~/Downloads;
-go get -u github.com/tomnomnom/assetfinder;
-cp ~/Downloads/bin/assetfinder .; 
-git clone https://github.com/m8r0wn/subscraper ; cd subscraper; sudo python3 setup.py install; cd .. ;
-git clone https://github.com/aboul3la/Sublist3r.git
-pip3 install -r Sublist3r/requirements.txt
 
 echo 'Subscraper Enumeration'
 python3 subscraper/subscraper.py "$TARGET" -o subscraper.txt;
