762085403996e8474cfcade32b42afa1d78d8f48
ValluvarSploit <73417621+ValluvarSploit@users.noreply.github.com>
2021-05-05 15:59:29 +0530

    Update subs.sh

--- subs.sh
+++ subs.sh
@@ -2,8 +2,8 @@
 
 TARGET=$1
 
-echo 'Subscraper Enumeration'
-python3 subscraper/subscraper.py "$TARGET" -o subscraper.txt;
+#echo 'Subscraper Enumeration'
+#python3 subscraper/subscraper.py "$TARGET" -o subscraper.txt;
 
 echo 'Sublist3r Enumeration'
 python3 Sublist3r/sublist3r.py -d "$TARGET" -t 10 -v -o sublist3r.txt > /dev/null          
